1a140b5c85cfc1ba64e28570289ca5086a818383
wiktoria250001 <149382337+wiktoria250001@users.noreply.github.com>
2024-01-29 22:16:30 +0100

    develop: create table

--- projekt serafin.py
+++ projekt serafin.py
@@ -11,3 +11,36 @@ db_params=ps.connect(
     port=5433
 )
 cursor=db_params.cursor()
+
+create_jednostki_policji='''
+    CREATE TABLE IF NOT EXISTS jednostki_policji (
+    id INT PRIMARY KEY,
+    nazwa TEXT(50),
+    lokalizacja TEXT(50)
+    );
+'''
+cursor.execute(create_jednostki_policji)
+db_params.commit()
+
+create_policjanci='''
+    CREATE TABLE IF NOT EXISTS policjanci (
+    id INT PRIMARY KEY,
+    imie TEXT(50),
+    nazwisko TEXT(50),
+    miejscowosc TEXT(50),
+    placowka TEXT(50)
+    );
+'''
+cursor.execute(create_policjanci)
+db_params.commit()
+
+create_incydenty='''
+    CREATE TABLE IF NOT EXISTS incydenty (
+    id INT PRIMARY KEY,
+    lokalizacja TEXT(50),
+    typ TEXT(50),
+    jednostka_policji TEXT(50)
+    );
+'''
+cursor.execute(create_incydenty)
+db_params.commit()
